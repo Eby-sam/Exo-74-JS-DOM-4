46fd3d30daede19a967ecdf5b1b2e9ef115cf780
coque <coquelet.samuel@gmail.com>
2021-11-18 10:52:58 +0100

    fini

--- exo 1/exercice1.js
+++ exo 1/exercice1.js
@@ -1,7 +1,11 @@
 //Votre code ci dessous
+let pet = document.getElementById('cat-pic');
 
+function atchoum() {
+    document.getElementById('cat-chat').innerHTML = "Miaou"
+}
 
-
+pet.addEventListener('click',atchoum);
 
 
 
--- exo 3/exercice.js
+++ exo 3/exercice.js
@@ -1,12 +1,13 @@
 let libButton = document.getElementById('lib-button');
 let libIt = function() {
     let storyDiv = document.getElementById("story");
-    storyDiv.innerHTML = "??Your story here";
+    let noun = document.getElementById('noun').value;
+    let adjective = document.getElementById('adjective').value;
+    let name = document.getElementById('person').value;
+
+    storyDiv.innerHTML = "Your story here with : " +name+ " who is " +adjective+ " and love " +noun;
 };
 libButton.addEventListener('click', libIt);
-
-
-
 
 
 
